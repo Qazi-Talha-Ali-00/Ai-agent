3fe4cd271d601110cc1f57e60487505d01da7ee2
Qazi-Talha-Ali-00 <b22cs087@iitj.ac.in>
2025-08-03 01:46:11 +0530

    new news collector

--- Collectors/news.py
+++ Collectors/news.py
@@ -1,19 +1,31 @@
 import feedparser
 from newspaper import Article
 
-rss_url = "https://news.google.com/rss"
+RSS_URL = "https://news.google.com/rss"
 
-feed = feedparser.parse(rss_url)
+def get_articles_from_rss(rss_url):
+    feed = feedparser.parse(rss_url)
+    articles = []
+    for entry in feed.entries[:5]:
+        try:
+            article = Article(entry.link)
+            article.download()
+            article.parse()
 
-for entry in feed.entries[:5]:  # Top 5 articles
-    print(f"\nFetching: {entry.title}")
-    url = entry.link
-    try:
-        article = Article(url)
-        article.download()
-        article.parse()
-        article.nlp()
-        print(f"Title: {article.title}")
-        print(f"Summary: {article.summary}")
-    except Exception as e:
-        print(f"Failed to process article: {e}")
+            articles.append({
+                "title": entry.title,
+                "summary": entry.summary,
+                "link": entry.link,
+                "published": entry.published,
+                "full_text": article.text
+            })
+        except Exception as e:
+            print(f"Failed to parse article: {entry.link} – {e}")
+    return articles
+
+if __name__ == "__main__":
+    full_articles = get_articles_from_rss(RSS_URL)
+    # for a in full_articles:
+    #     print(f"\n📌 {a['title']}\n➡️ {a['link']}\n📰 {a['full_text'][:5000]}...\n")
+    print(f"{full_articles[0]['title']}\n {full_articles[0]['full_text']}")
+    print(full_articles[0])
